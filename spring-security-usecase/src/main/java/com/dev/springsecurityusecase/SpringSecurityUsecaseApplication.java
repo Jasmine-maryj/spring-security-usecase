1c55d2fae266d2a3679a815c749cfa2c9101da9c
Jasmine Mary <jasminemary630@gmail.com>
2023-06-26 22:26:13 +0530

    First Commit

---
+++ spring-security-usecase/src/main/java/com/dev/springsecurityusecase/SpringSecurityUsecaseApplication.java
@@ -0,0 +1,13 @@
+package com.dev.springsecurityusecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityUsecaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityUsecaseApplication.class, args);
+	}
+
+}
---
+++ spring-security-usecase/src/test/java/com/dev/springsecurityusecase/SpringSecurityUsecaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.springsecurityusecase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityUsecaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
